9e22c584bac09bb12f706b42efc032702ee0a8cb
HeaDCraBee <theheadcrabz@gmail.com>
2020-06-21 21:33:00 +0300

    add tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,49 +1,54 @@
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import logic.GameController;
 import logic.Main;
+import model.CheckerType;
+import model.Model;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.BeforeAll;
-import org.opentest4j.AssertionFailedError;
+import model.MoveResult;
+import model.MoveType;
+import org.junit.Test;
 import view.Checker;
-
-import static model.Model.board;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import view.Tile;
 
 
-public class Tests extends Application {
 
-    Main n = new Main();
+import static org.junit.Assert.*;
 
-    @BeforeAll
-    public void start(Stage primaryStage){
-        Scene scene = new Scene(n.createScene());
-        primaryStage.setTitle("RUCheckers");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        checkBoard();
-        primaryStage.close();
-    }
+
+public class Tests {
 
     @Test
-    public void checkBoard(){
-        int x = 0;
-        int y = 0;
-        try {
-            y = 1;
-            assertTrue(board[0][1].hasChecker());
-            x = 4;
-            y = 4;
-            assertTrue(board[4][4].hasChecker());
-        }
-        catch (AssertionFailedError e){
-            System.err.println("There are no checkers on the field [" + x + "][" + y + "]");
-        }
+    public void test() {
+        Main main = new Main();
+        main.createScene();
+        Model model = main.getMainModel();
+        GameController game = main.getGame();
+        Tile[][] board = model.getBoard();
+        assertTrue(board[0][1].hasChecker());
+        assertFalse(board[0][0].hasChecker());
+        Checker checker1 = new Checker(CheckerType.BLACK, 2, 3);
+        board[2][3].setChecker(checker1);
+        Checker checker2 = new Checker(CheckerType.WHITE, 3, 4);
+        board[3][4].setChecker(checker2);
+        board[4][5].setChecker(null);
+        MoveResult result;
+
+        result = game.tryMove(checker1, 3, 4);
+        assertEquals(MoveType.NONE, result.getType());
+        result = game.tryMove(checker1, 4, 5);
+        assertEquals(MoveType.NONE, result.getType());
+        result = game.tryMove(checker2, 3, 4);
+        assertEquals(MoveType.NONE, result.getType());
+        result = game.tryMove(checker2, 4, 3);
+        assertEquals(MoveType.NORMAL, result.getType());
+
+        game.setTurn(false);
+
+        result = game.tryMove(checker1, 3, 4);
+        assertEquals(MoveType.NONE, result.getType());
+        result = game.tryMove(checker1, 1, 4);
+        assertEquals(MoveType.NONE, result.getType());
+        result = game.tryMove(checker1, 4, 5);
+        assertEquals(MoveType.KILL, result.getType());
     }
-
-
-
-
 }
